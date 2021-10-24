e0eb55f9587929e915597d9d38e957b9a4498867
Amir Afshari <4mir4fshari@Gmail.com>
2021-10-24 17:25:46 +0330

    Deploy website - based on 36db0469bfedfa5e251616abb95c8ba567a0e6db

--- assets/js/ff8da436.f0823600.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[538],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Automatic License Plate Detection","permalink":"/blog/license-plate-detection"}]}')}}]);
